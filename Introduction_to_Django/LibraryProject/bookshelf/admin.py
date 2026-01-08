1cd2a3995d8adb6094c524cca19c5d59486b5c97
fatihaosman <fatihaosman17@gmail.com>
2026-01-08 11:47:21 +0300

    Added CRUD operation markdown files

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Add search functionality for these fields
+    search_fields = ('title', 'author')
+    
+    # Optional: add filters by year
+    list_filter = ('publication_year',)
+
+# Register Book with the customized admin
+admin.site.register(Book, BookAdmin)
