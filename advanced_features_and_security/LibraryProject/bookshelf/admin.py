f5227c60c2e843ca834d4a1917d216600dae34e3
fatihaosman <fatihaosman17@gmail.com>
2026-01-24 07:22:32 +0300

    added a new app accounts

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Book
 
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 class BookAdmin(admin.ModelAdmin):
     # Fields to display in the list view
     list_display = ('title', 'author', 'publication_year')
@@ -13,3 +17,16 @@ class BookAdmin(admin.ModelAdmin):
 
 # Register Book with the customized admin
 admin.site.register(Book, BookAdmin)
+
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
