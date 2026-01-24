20b71f00e8fa48650f3e6a471e87e8095934d73a
fatihaosman <fatihaosman17@gmail.com>
2026-01-24 10:30:17 +0300

    can modofy books

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-
+from django.urls import path
+from . import views
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('relationship_app.urls')),
+]
+ 
+
+
+urlpatterns = [
+    path('books/', views.book_list, name='book_list'),
 ]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -13,3 +13,12 @@ def view_book(request):
 def edit_book(request, book_id):
     # code to edit book
     return render(request, 'bookshelf/edit_book.html')
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
