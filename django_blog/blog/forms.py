e697ce96b59ae38b3a886363c2dd545e6b236cc8
fatihaosman <fatihaosman17@gmail.com>
2026-02-13 13:26:10 +0300

    corrected  tags   on views.py, to show how its explictly used

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from.models  import Post
 from .models import Comment
 from .models import Post
+from taggit.forms import TagWidget
 
   # We are EXTENDING Django's default registration form
   # This form already has: username, password1, password2
@@ -33,6 +34,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content','tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
         
 
 
