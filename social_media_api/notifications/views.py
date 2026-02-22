9bd112faa915564acadbaf0de343820471bbb499
fatihaosman <fatihaosman17@gmail.com>
2026-02-22 23:26:09 +0300

    correcte to use generics in views

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,7 +1,8 @@
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import status, generics
+from rest_framework import status
 from django.contrib.contenttypes.models import ContentType
 
 from .models import Post, Like
@@ -14,26 +15,11 @@ class LikePostView(APIView):
     def post(self, request, pk):
         post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
-
         if not created:
-            return Response(
-                {"message": "You already liked this post."},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
+            return Response({"message": "Already liked"}, status=status.HTTP_400_BAD_REQUEST)
         if post.author != request.user:
-            Notification.objects.create(
-                recipient=post.author,
-                actor=request.user,
-                verb="liked your post",
-                content_type=ContentType.objects.get_for_model(post),
-                object_id=post.id
-            )
-
-        return Response(
-            {"message": "Post liked successfully."},
-            status=status.HTTP_201_CREATED
-        )
+            Notification.objects.create(recipient=post.author, actor=request.user, verb="liked your post", content_type=ContentType.objects.get_for_model(post), object_id=post.id)
+        return Response({"message": "Post liked"}, status=status.HTTP_201_CREATED)
 
 
 class UnlikePostView(APIView):
@@ -42,16 +28,7 @@ class UnlikePostView(APIView):
     def post(self, request, pk):
         post = generics.get_object_or_404(Post, pk=pk)
         like = Like.objects.filter(user=request.user, post=post).first()
-
         if not like:
-            return Response(
-                {"message": "You have not liked this post."},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
+            return Response({"message": "Not liked"}, status=status.HTTP_400_BAD_REQUEST)
         like.delete()
-
-        return Response(
-            {"message": "Post unliked successfully."},
-            status=status.HTTP_200_OK
-        )
+        return Response({"message": "Post unliked"}, status=status.HTTP_200_OK)
