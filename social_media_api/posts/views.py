e6915490d67bd3c0cb34fca1d1da3c20c50d19cc
fatihaosman <fatihaosman17@gmail.com>
2026-02-22 23:03:49 +0300

    fixixng eero on followers and unfollow

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -58,10 +58,8 @@ class FeedView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        followed_users = request.user.following.all()
-        posts = Post.objects.filter(
-            author__in=followed_users
-        ).order_by
+        following_users = request.user.following.all()  # ✅ variable name fixed
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')  # ✅ inline
 
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
