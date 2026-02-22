4f451a05d5430ca89b7241855ed8469b4318f6b3
fatihaosman <fatihaosman17@gmail.com>
2026-02-22 16:39:17 +0300

    used getuser model in registration serializer for reusability

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework.authtoken.models import Token
-from .models import User
+
+User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,10 +16,9 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
-        
-#So when someone registers → they already have a token.
+
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
 
