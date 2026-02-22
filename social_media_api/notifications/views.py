878f4f4408a7e2f36667c20e433d224296994d22
fatihaosman <fatihaosman17@gmail.com>
2026-02-22 23:28:56 +0300

    correcte to use generics in views

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
 from django.contrib.contenttypes.models import ContentType
 
 from .models import Post, Like
@@ -15,10 +15,19 @@ class LikePostView(APIView):
     def post(self, request, pk):
         post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
+
         if not created:
             return Response({"message": "Already liked"}, status=status.HTTP_400_BAD_REQUEST)
+
         if post.author != request.user:
-            Notification.objects.create(recipient=post.author, actor=request.user, verb="liked your post", content_type=ContentType.objects.get_for_model(post), object_id=post.id)
+            Notification.objects.create(
+                recipient=post.author,
+                actor=request.user,
+                verb="liked your post",
+                content_type=ContentType.objects.get_for_model(post),
+                object_id=post.id
+            )
+
         return Response({"message": "Post liked"}, status=status.HTTP_201_CREATED)
 
 
