8f7e7ed3fc102faad5c54ca5b7c53613c9f58069
fatihaosman <fatihaosman17@gmail.com>
2026-02-22 23:13:36 +0300

    added notification apps

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -52,7 +52,28 @@ class Comment(models.Model):
     def __str__(self):
         return f"Comment by {self.author.username}"
       
-      
+from django.conf import settings
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')  # Prevent duplicate likes
+
+    def __str__(self):
+        return f"{self.user.email} likes {self.post.title}"      
       
 
 # Model defines structure
