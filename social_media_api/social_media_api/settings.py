1da40c08e7c0575e3604e4a45eabb1e535f728af
fatihaosman <fatihaosman17@gmail.com>
2026-02-22 23:47:37 +0300

    updated setting

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'your_db_host',
+        'PORT': '5432',
     }
 }
 
@@ -142,3 +146,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
 }
+
+DEBUG = False  # Must be False in production
+ALLOWED_HOSTS = ['yourapp.herokuapp.com', 'www.yourdomain.com']  # Replace with your host
+
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True  # Only if using HTTPS
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
